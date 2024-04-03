b77444407b6687c9e458870e51e20fd1724fb9ab
Aristides Pascoal Queta <aristidesquetajr@gmail.com>
2024-04-03 09:48:31 +0100

    feat(web): configured tailwindcss

--- pass-in-web/src/app.tsx
+++ pass-in-web/src/app.tsx
@@ -1,3 +1,16 @@
+interface MeuBotaoProps {
+  texto: string
+}
+
+function MeuBotao(props: MeuBotaoProps) {
+  return <button className="bg-orange-400 h-10 px-3 rounded font-medium">{props.texto}</button>
+}
+
 export function App() {
-  return <h1>Hello World</h1>
+  return (
+    <div className="flex gap-2">
+      <MeuBotao texto="Clique aqui!" />
+      <MeuBotao texto="Botão 2" />
+    </div>
+  )
 }
