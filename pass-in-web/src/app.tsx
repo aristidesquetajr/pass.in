671d5e70165063d5c84c224d77ed8f9622b49282
Aristides Pascoal Queta <aristidesquetajr@gmail.com>
2024-04-03 10:18:21 +0100

    style(web): view body and header

--- pass-in-web/src/app.tsx
+++ pass-in-web/src/app.tsx
@@ -1,16 +1,11 @@
-interface MeuBotaoProps {
-  texto: string
-}
-
-function MeuBotao(props: MeuBotaoProps) {
-  return <button className="bg-orange-400 h-10 px-3 rounded font-medium">{props.texto}</button>
-}
+import { AttendeeList } from "./components/attendee-list";
+import { Header } from "./components/header";
 
 export function App() {
   return (
-    <div className="flex gap-2">
-      <MeuBotao texto="Clique aqui!" />
-      <MeuBotao texto="Botão 2" />
+    <div className="max-w-[1216px] mx-auto py-5">
+      <Header />
+      <AttendeeList />
     </div>
   )
 }
